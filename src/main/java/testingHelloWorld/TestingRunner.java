732fd86beac67189c010fc7666ae8038c17cdf35
mayvu <may.vu@onemount.com>
2022-03-01 15:34:21 +0700

    first commit

---
+++ src/main/java/testingHelloWorld/TestingRunner.java
@@ -0,0 +1,14 @@
+package testingHelloWorld;
+
+import java.util.Date;
+
+public class TestingRunner {
+    public static void  main(String[] args)
+    {
+        byte myAge = 18;
+        System.out.println("Em chưa " + myAge);
+        Date now = new Date();
+        System.out.println(now);
+    }
+
+}
