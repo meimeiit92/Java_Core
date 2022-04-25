f5e4a150564001ad9604a5a0192aab1c95822abc
mayvu <may.vu@onemount.com>
2022-04-25 12:00:35 +0700

    Nop bai Stream API

--- src/main/java/OOP/B14_Stream_API/BTVN/Main.java
+++ src/main/java/OOP/B14_Stream_API/BTVN/Main.java
@@ -3,7 +3,10 @@ package OOP.B14_Stream_API.BTVN;
 import OOP.B14_Stream_API.Student;
 import com.github.javafaker.Faker;
 
+import java.sql.SQLOutput;
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -43,8 +46,34 @@ public class Main {
         long soNguoi = lstHuman.stream().filter(item-> item.getAge()>20).filter(item->item.getAge()<30).count();
         System.out.println("SO luong nguoi co do tuoi tu 20-30 la " + soNguoi);
         // Dem so luong thanh pho
-       //lstHuman.stream().distinct().
+        Map<String, List<Human>> map = lstHuman.stream().collect(Collectors.groupingBy(new Function<Human, String>() {
+            @Override
+            public String apply(Human human) {
+                return human.getCity();
+            }
+        }));
+        // In ra danh sach City
+        for(Map.Entry<String,List<Human>> entry: map.entrySet()){
+            System.out.println("Thanh pho  " + entry.getKey());
+           // System.out.println("Thanh pho "+ entry.getValue());
+        }
+        // In ra so luong thanh pho
+        System.out.println("So luong thanh pho la " + map.size());
 
+        // Lay ra 1o nguoi dau tien trong danh sach
+        lstHuman.stream().filter(item -> item.getId()>=0).limit(10).forEach(i-> System.out.println(i + ""));
+        // Tinh do tuoi trung binh
+        int sum =  lstHuman.stream().mapToInt(value -> value.getAge()).sum();
+        System.out.println("do tuoi trung binh la " + sum/1000);
+        // Tinh muc luong trung binh cua Nam o do tuoi tu 20-40
+        long iafhia= lstHuman.stream().filter(item -> item.getGender()==1).filter(item -> item.getAge()>20)
+                .filter(item -> item.getAge()<40).count();
+        int sumSalary = lstHuman.stream().filter(item -> item.getGender()==1).filter(item -> item.getAge()>20)
+                .filter(item -> item.getAge()<40).mapToInt(value -> value.getSalary()).sum();
+        System.out.println("Muc luong turng binh cua nam o do tuoi tu 20-40 la "+ sumSalary/iafhia + "viet nam dong");
+        // Sap xep danh sach theo do tuoi
+        List<Human> sortHuman = lstHuman.stream().sorted((o1,o2) -> o1.getAge() - o2.getAge()).collect(Collectors.toList());
+        sortHuman.forEach(t-> System.out.println(t + ""));
 
     }
 }
